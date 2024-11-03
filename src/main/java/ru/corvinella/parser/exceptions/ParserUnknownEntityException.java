f226fa350e2846af5829106394d143e7b788f37a
Sh18RW <sh18rw@yandex.ru>
2024-11-03 18:25:02 +0300

    Parser exception comments.

--- src/main/java/ru/corvinella/parser/exceptions/ParserUnknownEntityException.java
+++ src/main/java/ru/corvinella/parser/exceptions/ParserUnknownEntityException.java
@@ -1,6 +1,12 @@
 package ru.corvinella.parser.exceptions;
 
 public class ParserUnknownEntityException extends ParserException {
+    /**
+     * Exception signalise that in expression unknown symbol and parser can't work with it.
+     * @param symbol a symbol which caused an exception.
+     * @param expression an expression which caused an exception.
+     * @param tracer an index of symbol which caused an exception.
+     */
     public ParserUnknownEntityException(String symbol, String expression, int tracer) {
         super(String.format("Unknown \"%s\" character", symbol), expression, tracer);
     }
